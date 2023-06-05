cca3faf1c8206a57826b467b12dda2e50ea5a7c6
RompeTecla <javalinogames@gmail.com>
2023-06-05 10:11:20 +0200

    Primeros pasos con EXPRESS

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+
+app.get('/health', (req,res) => {    //PRIMER ENDPOIUNT DE PRUEBA
+    return res.send('healthy');
+});
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server está corriendo en el PUERTO: " + PORT)
+});
