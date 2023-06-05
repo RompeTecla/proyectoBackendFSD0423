4ee136e04fb892cd641f634813ebb8dcb4983b2f
RompeTecla <javalinogames@gmail.com>
2023-06-05 21:05:02 +0200

    modificados index.js, package-lock.json, package.json

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
-const db = require("./db")
+const db = require("./db");
+const { User } = require('./models');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.json())
 
 app.get('/health', (req,res) => {    //PRIMER ENDPOINT DE PRUEBA
     return res.send('healthy');
 });
+
+
+app.post('/register', async(req,res) => {
+    console.log(req.body);
+
+    if(req.body.password.length < 4) {
+        return res.send('La pasword debe tener al menos 4 caracteres alfanuméricos')
+    }
+
+    const newPassword = bcrypt.hashSync(req.body.password, 8)
+
+    const newUser = await User.create(
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: newPassword,
+            role_id: 1
+        }
+    );
+
+
+    return res.send(newUser);
+})
+
+
+
 
 db.then(() => {
     app.listen(PORT, () => {
