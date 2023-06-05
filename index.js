312734f53195b3596975d608337a48aaf89debd2
RompeTecla <javalinogames@gmail.com>
2023-06-05 11:12:31 +0200

    creacion: conexión con la base de datos

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db")
 
 const app = express();
 
@@ -9,6 +10,18 @@ app.get('/health', (req,res) => {    //PRIMER ENDPOINT DE PRUEBA
     return res.send('healthy');
 });
 
-app.listen(3000, () => {
+db.then(() => {
+    app.listen(PORT, () => {
+        console.log('Server está corriendo en el puerto: ' + PORT)
+    })
+}).catch((error) => {
+    console.error("Error starting server", error.message);
+});
+
+
+
+
+
+app.listen(3001, () => {
     console.log("Server está corriendo en el PUERTO: " + PORT)
 });
