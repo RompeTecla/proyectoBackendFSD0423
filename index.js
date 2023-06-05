bfe02364ac733eca4c7e0079702a7bfb37a4f239
RompeTecla <javalinogames@gmail.com>
2023-06-05 10:22:25 +0200

    creacion: carpetas iniciales creadas con sequelize-cli

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const app = express();
 const PORT = 3000;
 
 
-app.get('/health', (req,res) => {    //PRIMER ENDPOIUNT DE PRUEBA
+app.get('/health', (req,res) => {    //PRIMER ENDPOINT DE PRUEBA
     return res.send('healthy');
 });
-
-
-
-
 
 app.listen(3000, () => {
     console.log("Server está corriendo en el PUERTO: " + PORT)
